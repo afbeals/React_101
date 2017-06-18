04ebddda90fa51931575de9c034183c0eb37c90b
Allan <afbeals@hotmail.com>
2017-06-18 10:46:38 -0700

    add two-way binding

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,7 +4,8 @@ export class Home extends React.Component {
 	constructor(props){
 		super();
 		this.state = {
-			age: props.initialAge
+			age: props.initialAge,
+			homeLink: props.initialLinkName
 		}
 	}
 
@@ -14,12 +15,25 @@ export class Home extends React.Component {
 		});
 	}
 
+	onChangeLink() {
+		this.props.changeLink(this.state.homeLink);
+	}
+
+	onHandleChange(e){
+		this.setState({
+			homeLink: e.target.value
+		})
+	}
+
 	render() {
 		return(
 			<div>
 				<p>My name is {this.props.name}, and I am {this.state.age} year's old</p>
 				<hr/>
-				<button className="btn btn-primary" onClick={this.onMakeOlder.bind(this)}>Make me Older</button>	
+				<button className="btn btn-primary" onClick={this.onMakeOlder.bind(this)}>Make me Older</button>
+				<hr />
+				<input type="text" value={this.state.homeLink} onChange={(e)=>this.onHandleChange(e)}/>
+				<button onClick={() => this.onChangeLink()} className="btn btn-primary">Change Header Link</button>
 			</div>	
 		);
 	}
--- src/app/index.js
+++ src/app/index.js
@@ -6,16 +6,34 @@ import {Home} from "./components/Home.js";
 import {SubText} from "./components/SubText.js";
 
 class App extends React.Component {
+	constructor(){
+		super();
+		this.state = {
+			homeLink: "Home"
+		}
+	}
+
+	onChangeLinkName(newName){
+		this.setState({
+			homeLink: newName
+		})
+	}
+
+
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-lg-12 col-md-12">
-						<Header />
+						<Header homeLink={this.state.homeLink} />
 					</div>
 					<hr className="col-lg-12 col-md-12"/>
 					<div className="col-lg-12 col-md-12">
-						<Home name={"Allan"} initialAge={28} />
+						<Home 	name={"Allan"} 
+								initialAge={28} 
+								changeLink={(e)=> this.onChangeLinkName(e)} 
+								initialLinkName={this.state.homeLink}
+						/>
 					</div>
 					<div className="col-lg-12 col-md-12">
 						<SubText />
