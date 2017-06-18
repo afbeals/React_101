ebd927e59c28f6428be0fa4eaca3b8db8acede54
Allan <afbeals@hotmail.com>
2017-06-17 19:16:31 -0700

    add stateless component

---
+++ src/app/components/SubText.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SubText = (props) => {
+	return(
+		<div className="col-lg-12">
+			<hr className="col-lg-12" />
+			<p>Stateless component</p>
+		</div>
+	)
+} 
--- src/app/index.js
+++ src/app/index.js
@@ -3,19 +3,22 @@ import { render } from "react-dom";
 
 import {Header} from "./components/Header.js";
 import {Home} from "./components/Home.js";
-
+import {SubText} from "./components/SubText.js";
 
 class App extends React.Component {
 	render() {
 		return (
 			<div className="container">
 				<div className="row">
-					<div className="col-lg-12 col-md-6">
+					<div className="col-lg-12 col-md-12">
 						<Header />
 					</div>
-					<hr className="col-lg-12 col-md-6"/>
-					<div className="col-lg-12 col-md-6">
+					<hr className="col-lg-12 col-md-12"/>
+					<div className="col-lg-12 col-md-12">
 						<Home name={"Allan"} initialAge={28} />
+					</div>
+					<div className="col-lg-12 col-md-12">
+						<SubText />
 					</div>
 				</div>
 			</div>
